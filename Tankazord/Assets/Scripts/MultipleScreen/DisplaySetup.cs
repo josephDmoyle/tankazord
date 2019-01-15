8d4d19c2b4897b1ad2a8bdb6b6b1347eed9ec6e0
Tezika <tezikazhou@gmail.com>
2019-01-15 10:32:00 -0700

    Finish the multiple screen

---
+++ Tankazord/Assets/Scripts/MultipleScreen/DisplaySetup.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+
+public class DisplaySetup : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        // Activate the display screen
+        if ( Display.displays.Length > 1 )
+        {
+            Display.displays[1].Activate();
+        }
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}

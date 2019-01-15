3ac17d0ab8383c3d6f1805cb5e1ee1f56d1e5c9f
josephDmoyle <u0929730@utah.edu>
2019-01-15 11:52:35 -0700

    Some changes

--- Tankazord/Assets/Scripts/Pilot.cs
+++ Tankazord/Assets/Scripts/Pilot.cs
@@ -5,20 +5,24 @@ using UnityEngine;
 public class Pilot : Player
 {
     [SerializeField] Transform hull, footL, footR;
-    [SerializeField] float sensitivity;
-    float turn = 0f, rx = 0f, ry = 0f, lx = 0f, ly = 0f;
-    Vector3 L, R;
-    // Start is called before the first frame update
+    [SerializeField] float turnSensitivity, footSensitivity;
+    float turn = 0f, rx = 0f, ry = 0f, lx = 0f, ly = 0f, ryGoal = 1f;
+    float timeStep = 1f, timerStep = 0f;
+    Rigidbody body;
+    [SerializeField]Vector3 vel;
+    private void Awake()
+    {
+        body = GetComponent<Rigidbody>();
+    }
+
     void Start()
     {
-        L = footL.transform.localPosition;
-        R = footR.transform.localPosition;
     }
 
     // Update is called once per frame
     void FixedUpdate()
     {
-        turn = Input.GetAxis("Turn") * sensitivity;
+        turn = Input.GetAxis("Turn") * turnSensitivity;
         rx = Input.GetAxis("RX");
         ry = Input.GetAxis("RY");
         lx = Input.GetAxis("LX");
@@ -27,10 +31,25 @@ public class Pilot : Player
 
         turn *= Time.fixedDeltaTime;
 
-        hull.Rotate(0, turn, 0);
+        transform.Rotate(0, turn, 0);
 
-        footL.localPosition = L + new Vector3(lx, 0f, ly);
+        if(vel == Vector3.zero)
+        {
+            if (ry == 1f && ly == -1f && ry == ryGoal)
+            {
+                timerStep = 0f;
+                ryGoal = -1f;
+                vel = transform.forward * footSensitivity;
+            }
+            else if (ly == 1f && ry == -1f && ry == ryGoal)
+            {
+                timerStep = 0f;
+                ryGoal = 1f;
+                vel = transform.forward * footSensitivity;
+            }
+        }
+        body.velocity = vel;
 
-        footR.localPosition = R + new Vector3(rx, 0f, ry);
+        vel = Vector3.Lerp(vel, Vector3.zero, timerStep += Time.fixedDeltaTime);
     }
 }

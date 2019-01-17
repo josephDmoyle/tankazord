3b19be3b0ba2c68e078e293287921685e256003d
Tezika <tezikazhou@gmail.com>
2019-01-16 20:41:01 -0700

    Add some placeholder materials and setup the test scene

---
+++ Tankazord/Assets/Scripts/Player/PlayerController.cs
@@ -0,0 +1,41 @@
+﻿using UnityEngine;
+
+namespace Princeps
+{
+    public class PlayerController : MonoBehaviour
+    {
+
+        public float moveSpeed;
+
+        public Rigidbody rigidbody { get; private set; }
+
+        public Camera mainCamera { get; private set; }
+
+        private void Awake()
+        {
+            this.rigidbody = this.GetComponent<Rigidbody>();
+            this.mainCamera = Camera.main;
+        }
+
+        // Start is called before the first frame update
+        void Start()
+        {
+
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+            // Get the mouse position on world coordinate
+            Vector3 mousePosition = mainCamera.ScreenToWorldPoint(Input.mousePosition);
+            this.transform.LookAt(mousePosition + Vector3.up * transform.position.y);
+
+        }
+
+        private void FixedUpdate()
+        {
+
+        }
+    }
+}
+
--- Tankazord/Assets/Scripts/View/FieldOfView.cs
+++ Tankazord/Assets/Scripts/View/FieldOfView.cs
@@ -1,6 +1,8 @@
-﻿using UnityEngine;
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
 
-public class EnemySpawner : MonoBehaviour
+public class FieldOfVIew : MonoBehaviour
 {
     // Start is called before the first frame update
     void Start()

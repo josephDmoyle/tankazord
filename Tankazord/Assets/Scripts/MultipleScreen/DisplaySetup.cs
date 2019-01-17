f130713c896ebd9597817ba698a9788d0b8d603d
Tezika <tezikazhou@gmail.com>
2019-01-16 19:28:35 -0700

    Change the name of dev scene

--- Tankazord/Assets/Scripts/MultipleScreen/DisplaySetup.cs
+++ Tankazord/Assets/Scripts/MultipleScreen/DisplaySetup.cs
@@ -5,7 +5,7 @@ public class DisplaySetup : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        // Activate the display screen
+        // Activate the display screen( Assuming that there are only three screens )
         if ( Display.displays.Length > 1 )
         {
             Display.displays[1].Activate();

101a53d81c89e7853e6c28320fbec9d7f7261e19
Tezika <tezikazhou@gmail.com>
2019-01-22 10:57:06 -0700

    Comment out the control for player

--- Tankazord/Assets/Scripts/Player/Captain.cs
+++ Tankazord/Assets/Scripts/Player/Captain.cs
@@ -36,18 +36,18 @@ namespace Princeps.Player
         // Update is called once per frame
         void Update()
         {
-            // Get the mouse position on world coordinate
-            Vector3 mouseWorldPos = mainCamera.ScreenToWorldPoint( new Vector3( Input.mousePosition.x, Input.mousePosition.y, mainCamera.transform.position.y ) );
-            this.transform.LookAt( mouseWorldPos + Vector3.up * transform.position.y );
-            // Update the velocity for player
-            this.velocity = new Vector3( Input.GetAxisRaw( "Horizontal" ), 0, Input.GetAxisRaw( "Vertical" ) ).normalized * this.moveSpeed;
+            //// Get the mouse position on world coordinate
+            //Vector3 mouseWorldPos = mainCamera.ScreenToWorldPoint( new Vector3( Input.mousePosition.x, Input.mousePosition.y, mainCamera.transform.position.y ) );
+            //this.transform.LookAt( mouseWorldPos + Vector3.up * transform.position.y );
+            //// Update the velocity for player
+            //this.velocity = new Vector3( Input.GetAxisRaw( "Horizontal" ), 0, Input.GetAxisRaw( "Vertical" ) ).normalized * this.moveSpeed;
             // Fire the Missile
             this.FireMissile( );
         }
 
         private void FixedUpdate()
         {
-            this.rigidbody.MovePosition( this.rigidbody.position + this.velocity * Time.fixedDeltaTime );
+            //this.rigidbody.MovePosition( this.rigidbody.position + this.velocity * Time.fixedDeltaTime );
         }
 
 

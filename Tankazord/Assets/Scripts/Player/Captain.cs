4a68a68e5aa1705af2d01544634213a6f06c0668
Tezika <tezikazhou@gmail.com>
2019-01-22 10:52:37 -0700

    Finish the logic about firing the missile

--- Tankazord/Assets/Scripts/Player/Captain.cs
+++ Tankazord/Assets/Scripts/Player/Captain.cs
@@ -1,4 +1,5 @@
-﻿using UnityEngine;
+﻿using System.Collections.Generic;
+using UnityEngine;
 
 namespace Princeps.Player
 {
@@ -14,6 +15,12 @@ namespace Princeps.Player
 
         public Missile missilePrefab;
 
+        public FieldOfView view;
+
+        public TargetManager targetManager;
+
+        private List<KeyCode> _keycodes;
+
         private void Awake()
         {
             this.rigidbody = this.GetComponent<Rigidbody>( );
@@ -23,7 +30,7 @@ namespace Princeps.Player
         // Start is called before the first frame update
         void Start()
         {
-
+            this.SetupKeycodes( );
         }
 
         // Update is called once per frame
@@ -34,6 +41,8 @@ namespace Princeps.Player
             this.transform.LookAt( mouseWorldPos + Vector3.up * transform.position.y );
             // Update the velocity for player
             this.velocity = new Vector3( Input.GetAxisRaw( "Horizontal" ), 0, Input.GetAxisRaw( "Vertical" ) ).normalized * this.moveSpeed;
+            // Fire the Missile
+            this.FireMissile( );
         }
 
         private void FixedUpdate()
@@ -41,9 +50,41 @@ namespace Princeps.Player
             this.rigidbody.MovePosition( this.rigidbody.position + this.velocity * Time.fixedDeltaTime );
         }
 
+
         private void FireMissile()
         {
+            // Hardcode the keycode first;
+            if ( Input.anyKeyDown )
+            {
+                foreach ( var key in _keycodes )
+                {
+                    if ( Input.GetKeyDown( key ) )
+                    {
+                        var targetName = "Target_" + key.ToString( ).Substring( 5 );
+                        var target = targetManager.targetPoints.Find( t => t.name == targetName );
+                        if ( target != null )
+                        {
+                            Instantiate<Missile>( this.missilePrefab, new Vector3( target.transform.position.x, this.transform.position.y + this.missilePrefab.instantiateHeight, target.transform.position.z), Quaternion.identity, null );
+                        }
+                        break;
+                    }
+                }
 
+            }
+        }
+
+        private void SetupKeycodes()
+        {
+            _keycodes = new List<KeyCode>( );
+            _keycodes.Add( KeyCode.Alpha1 );
+            _keycodes.Add( KeyCode.Alpha2 );
+            _keycodes.Add( KeyCode.Alpha3 );
+            _keycodes.Add( KeyCode.Alpha4 );
+            _keycodes.Add( KeyCode.Alpha5 );
+            _keycodes.Add( KeyCode.Alpha6 );
+            _keycodes.Add( KeyCode.Alpha7 );
+            _keycodes.Add( KeyCode.Alpha8 );
+            _keycodes.Add( KeyCode.Alpha9 );
         }
     }
 }
